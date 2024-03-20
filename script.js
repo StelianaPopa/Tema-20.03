d88ee6a8bd475fb62b2f52c5531cdc51ade74032
Popa Steliana <stelianagurgulin43@gmail.com>
2024-03-20 11:28:28 +0200

    tema reviewed

--- script.js
+++ script.js
@@ -5,16 +5,22 @@
 const valoareImparaMaxima = (numere) => {
     let imparMaxim = numere[0];
     for (let i = 1; i < numere.length; i++) {
+        if (numere[i] % 2 !== 0){
+            imparMaxim = numere[i];
+        }
+    };
+    for (let i = 1; i < numere.length; i++) {
         if (numere[i] % 2 !== 0 && numere[i] > imparMaxim) {
             imparMaxim = numere[i];
         }
-    }
+    };
     if (imparMaxim % 2 !== 0){
         return imparMaxim;
     }
 };
 console.log(valoareImparaMaxima([5, 7, 2, 3, 10]));
 console.log(valoareImparaMaxima([2, 4]));
+console.log(valoareImparaMaxima([2,1,4]));
 
 
 // Creează o funcție care primește ca parametru un array de numere și 
@@ -25,17 +31,24 @@ const indexImparMaxim = (numere) => {
     let imparMaxim = numere[0];
     let index = 0; 
     for (let i = 1; i < numere.length; i++) {
+        if (numere[i] % 2 !== 0){
+            imparMaxim = numere[i];
+            index = i;
+        }
+    }; 
+    for (let i = 1; i < numere.length; i++) {
         if (numere[i] % 2 !== 0 && numere[i] > imparMaxim) {
             imparMaxim = numere[i];
             index = i;
         }
-    }
+    };
     if (imparMaxim % 2 !== 0) {
         return index;
     }
 };
 console.log(indexImparMaxim([5, 7, 2, 3, 10]));
 console.log(indexImparMaxim([2, 4, 6]));
+console.log(indexImparMaxim([2, 1, 4]));
 
 // Creează o funcție care primește ca parametru un array de numere și 
 // returnează valoare minima din array, dar nu mai mică decât zero. 
@@ -54,6 +67,7 @@ const valoareMinimaPozitiva = (numere) => {
 };
 console.log(valoareMinimaPozitiva([5, -7, 2, 3, -1, 10]));
 console.log(valoareMinimaPozitiva([-5, -7, -2, -3, -1, -10]));
+
 
 // Creează o funcție care primește ca parametru un array de numere și 
 // returnează diferența dintre maximul și minimul din array.
